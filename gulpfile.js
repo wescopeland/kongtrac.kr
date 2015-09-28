181718158ae6235f9f64c736f0c09896fbca972a
Wes Copeland <wlcopeland1@gmail.com>
2015-09-28 13:06:27 -0400

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp         = require('gulp'),
+    rename       = require('gulp-rename'),
+    jshint       = require('gulp-jshint'),
+    stylish      = require('jshint-stylish'),
+    ngAnnotate   = require('gulp-ng-annotate'),
+    uglify       = require('gulp-uglify'),
+    concat       = require('gulp-concat'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    del          = require('del'),
+    bust         = require('gulp-buster'),
+    watch        = require('gulp-watch');
+
+gulp.task('scripts', function() {
+
+	return gulp.src(['./client/app/**/*.module.js', './client/app/**/*.js', '!./client/app/**/*.spec.js'])
+	    .pipe(jshint())
+	    .pipe(jshint.reporter(stylish))
+	    //.pipe(sourcemaps.init())
+	        //.pipe(concat('app.min.js', {newLine: ';'}))
+	        .pipe(ngAnnotate({add: true}))
+	        //.pipe(uglify({mangle: true}))
+	    //.pipe(sourcemaps.write())
+	    .pipe(gulp.dest('./client/dist/js'))
+	    //.pipe(bust())
+	    //.pipe(gulp.dest('./client'))
+
+});
+
+// TODO: Vendor task.
+
+// TODO: Tests task.
+
+gulp.task('clean', function(callback) {
+	del(['./client/dist'], callback);
+});
+
+gulp.task('default', ['clean'], function() {
+	gulp.start('scripts');
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./client/app/**/*.js', ['scripts']);
+});
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var compress = require('compression')();
+var serveStatic = require('serve-static');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(compress);
+
+app.set('port', (process.env.PORT || 1337));
+app.use(serveStatic('client', {'index': ['index.html', 'index.htm']}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/bower_components'));
+
+app.listen(app.get('port'), function() {
+	console.log('dksta.ts server is now running at port ' + app.get('port'));
+});
+
+// Add headers
+app.use(function(req, res, next) {
+
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+	res.setHeader('Access-Control-Allow-Methods', 'GET');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	next();
+
+});
